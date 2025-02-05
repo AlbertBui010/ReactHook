d414510aebf28fc6ca7b9eaccc1faab79310c4db
AlbertBui010 <buiquangquy12823@gmail.com>
2025-02-05 22:49:20 +0700

    DOM Events

--- Chapter2/base-react-redux/src/App.js
+++ Chapter2/base-react-redux/src/App.js
@@ -3,24 +3,47 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-const App = () => {
-	const count = useSelector((state) => state.counter.count);
-	const dispatch = useDispatch();
+import React from 'react';
 
-	return (
-		<div className="App">
-			{/* <header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<div>Count = {count}</div>
-				<button onClick={() => dispatch(increaseCounter())}>Increase</button>
-				<button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-			</header> */}
-			<MyComponent />
-		</div>
-	);
-};
+class App extends React.Component {
+	state = {
+		name: 'Bui Quang Quy',
+		address: 'HCMC',
+		age: '21',
+	};
+
+	handleClickMe(event) {
+		console.log(event.target);
+	}
+
+	render() {
+		return (
+			<>
+				this is web page {this.state.name}
+				<button onClick={this.handleClickMe}>Click me</button>
+			</>
+		);
+	}
+}
+
+// const App = () => {
+// 	const count = useSelector((state) => state.counter.count);
+// 	const dispatch = useDispatch();
+
+// 	return (
+// 		<div className="App">
+// 			{/* <header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<div>Count = {count}</div>
+// 				<button onClick={() => dispatch(increaseCounter())}>Increase</button>
+// 				<button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+// 			</header> */}
+// 			<MyComponent />
+// 		</div>
+// 	);
+// };
 
 export default App;
---
+++ notes.md
@@ -0,0 +1,3 @@
+# State
+
+-  La mot object
